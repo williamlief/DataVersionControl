bf73830addbcc280884a8a107c495f7f5fa32c9f
Lief <williamlief@gmail.com>
2019-11-11 21:53:29 -0800

    Added support for stata dta files
    fixes #7

---
+++ R/dta.R
@@ -0,0 +1,82 @@
+#' Read Stata Files, using pkg foreign
+#'
+#' This is a wrapper function for `foreign::read.dta`, use is identical to the original function.
+#'
+#' Internally it checks the data version control file and returns the stamp associated
+#' with the current version of the desired `file`.
+#'
+#' @param file a filename or URL as a character string.
+#' @param ... other params for read.dta see \code{\link[foreign]{read.dta}}
+#' @param version_control name of data version control file. Default is `data_vc`. Should match name used in `make_data_vc`.
+#' @param verbose logical, report returned current file version? Default is FALSE
+#'
+#' @seealso \code{\link[foreign]{read.dta}}
+#'
+#' @return A data frame with attributes.
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' make_data_vc()
+#' foreign::write.dta_vc(cars, "my_cars.dta", verbose = TRUE)
+#' foreign::read.dta_vc("my_cars.dta", verbose = TRUE)
+#' }
+read.dta_vc <- function(file,
+                       ...,
+                       version_control = "DATA_VC",
+                       verbose = FALSE) {
+
+  if(!requireNamespace("foreign", quietly = TRUE)) {
+    stop("Package \"foreign\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+
+  file <- get_current_version(file, version_control, verbose)
+  foreign::read.dta(file, ...)
+
+}
+
+
+#' Write Stata Files, using pkg foreign
+#'
+#' This is a wrapper for `foreign::write.dta()`, use is identical to the original function.
+#'
+#' Internally it checks for the data version control file, and inserts or updates
+#' the record for the file that you are saving. The tracking is applied to the `file`
+#'
+#' @param dataframe a data frame
+#' @param file character string giving filename.
+#' @param ... other params for write.dta see \code{\link[foreign]{write.dta}}
+#' @param stamp file suffix to disambiguate, default is Sys.date()
+#' @param version_control name of data version control file. Default is `data_vc`. Should match name used in `make_data_vc`.
+#' @param verbose logical, report update to version control file? Default is FALSE
+#'
+#' @seealso \code{\link[foreign]{write.dta}}
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' make_data_vc()
+#' foreign::write.dta_vc(cars, "my_cars.dta", verbose = TRUE)
+#' }
+save.dta_vc  <- function(object,
+                       file,
+                       ...,
+                       stamp = Sys.Date(),
+                       version_control = "DATA_VC",
+                       verbose = FALSE
+) {
+
+  if(!requireNamespace("foreign", quietly = TRUE)) {
+    stop("Package \"foreign\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+
+  stamp <- paste(stamp)
+  update_version_control(file, stamp, version_control, verbose)
+  file_stamped <- stamp_file(file, stamp)
+
+  foreign::write.dta(object, file_stamped, ...)
+}
---
+++ tests/testthat/test_dta.R
@@ -0,0 +1,15 @@
+context("test foreign dta wrappers")
+
+save.dta_vc(cars[1:10,], "testdata/mycars.dta", stamp = "test2")
+save.dta_vc(cars[1:5,], "testdata/mycars.dta", stamp = "test3")
+
+# note teardown deletes everything in testdata directory
+
+test_that("confirm both test1 and test2 stamps exist", {
+  expect_true(file.exists("testdata/mycars_test1_.rds"))
+  expect_true(file.exists("testdata/mycars_test2_.dta"))
+})
+
+test_that("read gets latest version with only five rows", {
+  expect_equal(nrow(read.dta_vc("testdata/mycars.dta")), 5)
+})
