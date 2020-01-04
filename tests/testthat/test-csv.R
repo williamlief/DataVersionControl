21d596a15252276c9ad54fb4eed9eedd174817f1
Lief <williamlief@gmail.com>
2020-01-04 15:12:49 -0800

    fixed test statement to test correct function

--- tests/testthat/test-csv.R
+++ tests/testthat/test-csv.R
@@ -20,5 +20,5 @@ write_csv_vc(cars[1:5,], file, stamp = "test3")
 test_that("confirm write_csv_vc overwrites a write.csv created data_vc entry", {
   expect_true(file.exists("testdata/mycars_test2_.csv"))
   expect_true(file.exists("testdata/mycars_test3_.csv"))
-  expect_equal(nrow(read.csv_vc(file)), 5)
+  expect_equal(nrow(read_csv_vc(file)), 5)
 })
